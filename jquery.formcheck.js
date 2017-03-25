9c190f7cf8edf9b92baea7d6c5f358f47f582500
morosefrog <morosefrog@qq.com>
2017-03-26 00:37:36 +0800

    增加提交前beforeSubmit方法，改进onBlur功能，版本 1.1.6

--- jquery.formcheck.js
+++ jquery.formcheck.js
@@ -1,7 +1,7 @@
 /**
  * Plug-In name: jquery.formCheck.js
- * Versions: 1.1.5
- * Modify time: 2017/02/17
+ * Versions: 1.1.6
+ * Modify time: 2017/03/25
  * Created by TomnTang on 2016/07/18
  * Website: http://www.lovevivi.com/plugin/jquery.formcheck.js/
  */
@@ -14,86 +14,87 @@
             items: {},
             pass: false,
             passSubmit: null,
+            beforeSubmit: null,
             ajaxList: {}
         };
 
         win.formCheck = {
-            ver: 'Versions: 1.1.4',
-            time: 'Modify Time: 2016/11/14'
+            ver: 'Versions: 1.1.6',
+            time: 'Modify Time: 2017/03/25'
         };
 
         var reg = {
-            required: function(obj){    //判断不为空，返回true
+            required: function(obj){ // 判断不为空，返回true
                 return (obj.value == '') ? false : true;
             },
-            empty: function(obj){    //判断为空，返回true
+            empty: function(obj){ // 判断为空，返回true
                 return (obj.value == '') ? true : false;
             },
-            text: function(obj){    //判断是否全是中文、英文、数字
+            text: function(obj){ // 判断是否全是中文、英文、数字
                 var regular = /^[0-9A-Za-z\u4E00-\u9FA5]+$/;
                 return regular.test(obj.value) ? true : false;
             },
-            number: function(obj){    //判断是否全是英文、数字
+            number: function(obj){ // 判断是否全是英文、数字
                 var regular = /^[0-9A-Za-z]+$/;
                 return regular.test(obj.value) ? true : false;
             },
-            digits: function(obj){    //判断是否全是数字
+            digits: function(obj){ // 判断是否全是数字
                 var regular = /^[0-9]+$/;
                 return regular.test(obj.value) ? true : false;
             },
-            money: function(obj){    //判断是浮点数或整数
+            money: function(obj){ // 判断是浮点数或整数
                 var regular = /^(\d+|\d+\.\d{1,2})$/;
                 return regular.test(obj.value) ? true : false;
             },
-            email: function(obj){    //判断是否为Email
+            email: function(obj){ // 判断是否为Email
                 var regular = /^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$/;
                 return regular.test(obj.value) ? true : false;
             },
-            url: function(obj){    //判断是否为URL地址
+            url: function(obj){ // 判断是否为URL地址
                 var regular = /^\b(((https?|ftp):\/\/)?[-a-z0-9]+(\.[-a-z0-9]+)*\.(?:com|net|org|cn|biz|info|name|[a-z][a-z]|((25[0-5])|(2[0-4]\d)|(1\d\d)|([1-9]\d)|\d))(:\d{4})?\b(\/[-a-z0-9_:\@&?=+,.!\/~%\$]*)?)$/i;
                 return regular.test(obj.value) ? true : false;
             },
-            icard: function(obj){    //判断15位或18位身份证格式是否正确
+            icard: function(obj){ // 判断15位或18位身份证格式是否正确
                 var regular = /(^\d{15}$)|(^\d{17}(\d|x)$)/i;
                 return regular.test(obj.value.toUpperCase()) ? true : false;
             },
-            mac: function(obj){    //判断是否是MAC格式
+            mac: function(obj){ // 判断是否是MAC格式
                 var regular = /(^[A-F\d]{2}[A-F\d]{2}[A-F\d]{2}[A-F\d]{2}[A-F\d]{2}[A-F\d]{2}$)|(^[A-F\d]{2}(-|:)[A-F\d]{2}(-|:)[A-F\d]{2}(-|:)[A-F\d]{2}(-|:)[A-F\d]{2}(-|:)[A-F\d]{2}$)/;
                 return regular.test(obj.value.toUpperCase()) ? true : false;
             },
-            ip: function(obj){    //判断是否是ip格式
+            ip: function(obj){ // 判断是否是ip格式
                 var regular = /^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$/;
                 return regular.test(obj.value) ? true : false;
             },
-            mobile: function(obj){    //判断是否是固话手机格式
+            mobile: function(obj){ // 判断是否是固话手机格式
                 var regular = /(^(\d{3,4})?(-)?\d{7,8}$)|(^1\d{10}$)/;
                 return regular.test(obj.value) ? true : false;
             },
-            chinese: function(obj){    //判断是否全是中文
+            chinese: function(obj){ // 判断是否全是中文
                 var regular = /^[\u4E00-\u9FA5]+$/;
                 return regular.test(obj.value) ? true : false;
             },
-            english : function(obj){    //判断是否全是英文
+            english : function(obj){ // 判断是否全是英文
                 var regular = /^[A-Za-z]+$/;
                 return regular.test(obj.value) ? true : false;
             },
-            textarea : function(obj){    //判断是否全是中文、英文、数字、\n换行符
+            textarea : function(obj){ // 判断是否全是中文、英文、数字、\n换行符
                 var regular = /^[0-9A-Za-z\u4E00-\u9FA5\n]+$/;
                 return regular.test(obj.value) ? true : false;
             },
-            checked: function(obj){    //判断是否checked
+            checked: function(obj){ // 判断是否checked
                 return (obj.checked) ? true : false;
             },
-            minlength: function(obj){    //判断最小长度为min
+            minlength: function(obj){ // 判断最小长度为min
                 return (obj.value.length >= obj.minlength) ? true : false;
             },
-            maxlength: function(obj){    //判断最大长度为max
+            maxlength: function(obj){ // 判断最大长度为max
                 return (obj.value.length <= obj.maxlength) ? true : false;
             },
-            min: function(obj){    //判断最小数值为min
+            min: function(obj){ // 判断最小数值为min
                 return (obj.value >= obj.min) ? true : false;
             },
-            max: function(obj){    //判断最大数值为max
+            max: function(obj){ // 判断最大数值为max
                 return (obj.value <= obj.max) ? true : false;
             }
         };
@@ -130,8 +131,9 @@
             obj.success = value.success || '检测通过';
             obj.onError = value.onError || null;
             obj.onSuccess = value.onSuccess || null;
-            obj.ajaxURL = value.ajax ? value.ajax.url : '';
-            obj.callback = value.ajax ? value.ajax.callback : null;
+            obj.ajaxType = (value.ajax && value.ajax.type) ? value.ajax.type : 'POST';
+            obj.ajaxURL = (value.ajax && value.ajax.url) ? value.ajax.url : '';
+            obj.callback = (value.ajax && value.ajax.callback) ? value.ajax.callback : null;
             obj.onoff = value.onoff || null;
 
             if (obj.onoff) {
@@ -194,7 +196,7 @@
                                 }
                             }, 1000);
                         } else {
-                            (obj.onSuccess) && obj.onSuccess(that);
+                            obj.onSuccess && obj.onSuccess(that);
                             debug.log('getEvent= '+ $(document.activeElement).context.type);
                             if ($(document.activeElement).context.type == 'submit') {
                                 that.parents('form').submit();
@@ -202,7 +204,6 @@
                         }
 
                         that.addClass('success').removeClass('error');
-
                         debug.log('ajax完成后，表单再次自动提交！');
                     }
                 }
@@ -211,13 +212,14 @@
             var isCallback = null, timer = null, loading = null, argData = {}, returnData = '';
             if (settings.ajaxList[id]) {
                 debug.log('settings.ajaxList[id]= '+ JSON.stringify(settings.ajaxList[id]));
+
                 if (settings.ajaxList[id].value !== obj.value) {
                     loading = layer.load(0);
                     settings.ajaxList[id].value = obj.value;
                     argData[id] = obj.value;
                     debug.log(argData);
                     $.ajax({
-                        type: 'POST',
+                        type: obj.ajaxType,
                         url: obj.ajaxURL,
                         data: argData,
                         dataType: 'json',
@@ -272,6 +274,14 @@
                         });
                     }
                 });
+            } else {
+            	$.each(settings.items, function(id, value){
+                    if (value.onBlur != true) {
+                        $('#'+id).on('blur', function(){
+                            verify(id, value, event);
+                        });
+                    }
+                });
             }
 
             $.each(settings.items, function(id, value){
@@ -295,6 +305,7 @@
         return this.each(function(){
             init();
             $(this).on('submit', function(){
+            	settings.beforeSubmit && settings.beforeSubmit();
                 check();
                 if (status) {
                     debug.info('表单检测通过！');
